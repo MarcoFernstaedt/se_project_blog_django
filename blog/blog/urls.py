11ab5bf9736c309937cab0bb4589d5b9ba7a121b
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-10-02 15:47:03 -0700

    implament: path for findPost view

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from post import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
+    path('<int:id>', views.findPost)
 ]
